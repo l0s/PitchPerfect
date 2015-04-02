606bf61862bc7c83692e27f376e296b1ed1374e8
Carlos Macasaet <github@carlos-m.net>
2015-04-01 23:43:29 -0700

    Lesson 4a: Recording Audio > Segue from Code

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -15,6 +15,24 @@ class RecordSoundsViewController: UIViewController {
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var stopButton: UIButton!
 
+    lazy var fileName:String = {
+        let scratchDirectory =
+            NSSearchPathForDirectoriesInDomains( .DocumentDirectory, .UserDomainMask, true )[ 0 ] as String
+        return scratchDirectory + "/pitch-perfect-recording.wav";
+    }()
+    lazy var session:AVAudioSession = {
+        return AVAudioSession.sharedInstance()
+    }()
+    lazy var recorder:AVAudioRecorder = {
+        var error:NSError?
+        println( "-- saving to: \( self.fileName )" )
+        let retval =
+            AVAudioRecorder( URL:NSURL( fileURLWithPath:self.fileName ), settings:nil, error:&error )
+        retval.meteringEnabled = true
+        assert( error == nil, "Error creating audio recorder: \( error!.localizedDescription )" )
+        return retval
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -27,6 +45,10 @@ class RecordSoundsViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
+    override func viewWillDisappear(animated: Bool) {
+//        NSFileManager.defaultManager().removeItemAtPath(fileName, error:nil )
+    }
+
     @IBAction func record(sender: AnyObject, forEvent event: UIEvent) {
         statusLabel.hidden = false
 
@@ -34,13 +56,21 @@ class RecordSoundsViewController: UIViewController {
 
         stopButton.hidden = false
         stopButton.enabled = true
+
+        var error:NSError?
+        session.setCategory( AVAudioSessionCategoryPlayAndRecord, error:&error )
+        assert( error == nil, "Error setting audio session category: \( error!.localizedDescription )" )
+
+        recorder.prepareToRecord()
+        recorder.record()
     }
 
     @IBAction func stop(sender: AnyObject, forEvent event: UIEvent) {
+        recorder.stop()
+        session.setActive( false, error: nil )
+
         stopButton.enabled = false
-
         statusLabel.hidden = true
-
         recordButton.enabled = true
     }
 
