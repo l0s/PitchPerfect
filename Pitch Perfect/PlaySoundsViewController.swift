fa98fe6513dc762028fd91c949295da7277d5160
Carlos Macasaet <github@carlos-m.net>
2015-04-01 18:21:21 -0700

    Lesson 3b: Playing Audio > Preparing to Play Sound

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Carlos Macasaet on 1/4/15.
+//  Copyright (c) 2015 Carlos Macasaet. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    @IBAction func playbackSlowly(sender: UIButton, forEvent event: UIEvent) {
+    }
+
+}
