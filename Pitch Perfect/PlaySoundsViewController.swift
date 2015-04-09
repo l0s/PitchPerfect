9fbc4d75790cdd7b5dd93118c2032aa9c55a6cd2
Carlos Macasaet <github@carlos-m.net>
2015-04-08 22:26:25 -0700

    Lesson 4b: Chipmunk & DarthVader effects > Putting it all together

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -12,17 +12,26 @@ import AVFoundation
 class PlaySoundsViewController: UIViewController {
 
     var audio:RecordedAudio!
-    var playbackError:NSError?
-    lazy var mainBundle:NSBundle = {
+    lazy var mainBundle:NSBundle! = {
         return NSBundle.mainBundle()
     }()
-    lazy var player:AVAudioPlayer = {
+    lazy var player:AVAudioPlayer! = {
         var error:NSError?
         let player =
-            AVAudioPlayer( contentsOfURL: self.audio.path, error: &self.playbackError )
+            AVAudioPlayer( contentsOfURL: self.audio.path, error: &error )
         player.enableRate = true
+        assert( error == nil )
         return player
     }()
+    lazy var engine:AVAudioEngine! = {
+        return AVAudioEngine()
+    }()
+    var file:AVAudioFile!
+
+    override func viewDidLoad() {
+        file = AVAudioFile( forReading: audio.path, error: nil )
+        println( "-- will play: \(file)" )
+    }
 
     override func viewWillDisappear( animated: Bool ) {
         player.stop()
@@ -30,15 +39,43 @@ class PlaySoundsViewController: UIViewController {
 
     @IBAction func playbackSlowly( sender: UIButton, forEvent event: UIEvent )
     {
-        playback( 0.5 )
+        playback( 0.5, pitch:1.0 )
     }
 
     @IBAction func playbackQuickly( sender: UIButton, forEvent event: UIEvent )
     {
-        playback( 2.0 )
+        playback( 2.0, pitch:1.0 )
     }
 
-    @IBAction func playbackLikeChipmunk(sender: UIButton, forEvent event: UIEvent) {
+    @IBAction func playbackLikeChipmunk( sender: UIButton, forEvent event: UIEvent ) {
+        player.stop()
+        engine.stop()
+        engine.reset()
+
+        println( "-- playing like a chipmunk" )
+        let node = AVAudioPlayerNode()
+
+        let effect = AVAudioUnitTimePitch()
+        effect.pitch = 1000
+
+        engine.attachNode( node )
+        engine.attachNode( effect )
+
+        println( "-- connecting pipes" )
+        engine.connect( node, to: effect, format: nil )
+        engine.connect( effect, to: engine.outputNode, format: nil )
+
+        println( "-- preparation" )
+        node.scheduleFile( file, atTime: nil, completionHandler: nil )
+
+        engine.prepare()
+        var engineError:NSError?
+        let engineStarted = engine.startAndReturnError( &engineError )
+        assert( engineError == nil )
+        assert( engineStarted )
+
+        println( "-- playback" )
+        node.play()
     }
 
     @IBAction func stop( sender: UIButton, forEvent event: UIEvent )
@@ -47,16 +84,13 @@ class PlaySoundsViewController: UIViewController {
         player.currentTime = 0
     }
 
-    func playback( rate:Float )
+    func playback( rate:Float, pitch:Float )
     {
         player.stop()
         player.rate = rate
+        
         let initiated = player.play()
         assert( initiated, "playback not initiated" )
-        if let error = playbackError
-        {
-            assert( false, "error occurred: \(error.description)" )
-        }
     }
 
 }
